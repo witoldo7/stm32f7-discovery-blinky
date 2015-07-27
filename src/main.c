a73fae92c93e2b30213c29c599047691568245a0
Kentaro Tanaka <spiralray@rakusei.net>
2015-07-27 09:48:13 +0900

    Add function of TCP and UDP echo server

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
 /**
   ******************************************************************************
-  * @file    LwIP/LwIP_HTTP_Server_Netconn_RTOS/Src/main.c 
+  * @file    LwIP/LwIP_HTTP_Server_Netconn_RTOS/Src/main.c
   * @author  MCD Application Team
   * @version V1.0.0
   * @date    25-June-2015
-  * @brief   This sample code implements a http server application based on 
-  *          Netconn API of LwIP stack and FreeRTOS. This application uses 
-  *          STM32F7xx the ETH HAL API to transmit and receive data. 
+  * @brief   This sample code implements a http server application based on
+  *          Netconn API of LwIP stack and FreeRTOS. This application uses
+  *          STM32F7xx the ETH HAL API to transmit and receive data.
   *          The communication is done with a web browser of a remote PC.
   ******************************************************************************
   * @attention
@@ -19,8 +19,8 @@
   *
   *        http://www.st.com/software_license_agreement_liberty_v2
   *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   * See the License for the specific language governing permissions and
   * limitations under the License.
@@ -52,6 +52,9 @@ static void Netif_Config(void);
 static void MPU_Config(void);
 static void CPU_CACHE_Enable(void);
 
+void tcpecho_init(void);
+void udpecho_init(void);
+
 /* Private functions ---------------------------------------------------------*/
 
 /**
@@ -73,49 +76,55 @@ int main(void)
        - Set NVIC Group Priority to 4
        - Global MSP (MCU Support Package) initialization
      */
-  HAL_Init();  
-  
+  HAL_Init();
+
   /* Configure the system clock to 216 MHz */
-  SystemClock_Config(); 
-  
+  SystemClock_Config();
+
   /* Init thread */
 #if defined(__GNUC__)
   osThreadDef(Start, StartThread, osPriorityNormal, 0, configMINIMAL_STACK_SIZE * 5);
 #else
   osThreadDef(Start, StartThread, osPriorityNormal, 0, configMINIMAL_STACK_SIZE * 2);
 #endif
-  
+
   osThreadCreate (osThread(Start), NULL);
-  
+
   /* Start scheduler */
   osKernelStart();
-  
+
   /* We should never get here as control is now taken by the scheduler */
   for( ;; );
 }
 
 /**
-  * @brief  Start Thread 
+  * @brief  Start Thread
   * @param  argument not used
   * @retval None
   */
 static void StartThread(void const * argument)
-{ 
+{
   /* Initialize LCD and LEDs */
   BSP_Config();
-  
+
   /* Create tcp_ip stack thread */
   tcpip_init(NULL, NULL);
-  
+
   /* Initialize the LwIP stack */
   Netif_Config();
-  
+
   /* Initialize webserver demo */
   http_server_netconn_init();
-  
+
+  /* Initialize tcp echo server */
+  tcpecho_init();
+
+  /* Initialize udp echo server */
+  udpecho_init();
+
   /* Notify user about the network interface config */
   User_notification(&gnetif);
-  
+
 #ifdef USE_DHCP
   /* Start DHCPClient */
 #if defined(__GNUC__)
@@ -123,13 +132,13 @@ static void StartThread(void const * argument)
 #else
   osThreadDef(DHCP, DHCP_thread, osPriorityBelowNormal, 0, configMINIMAL_STACK_SIZE * 2);
 #endif
-  
+
   osThreadCreate (osThread(DHCP), &gnetif);
 #endif
 
   for( ;; )
   {
-    /* Delete the Init Thread */ 
+    /* Delete the Init Thread */
     osThreadTerminate(NULL);
   }
 }
@@ -143,31 +152,31 @@ static void Netif_Config(void)
 {
   struct ip_addr ipaddr;
   struct ip_addr netmask;
-  struct ip_addr gw;	
-  
+  struct ip_addr gw;
+
   /* IP address setting */
   IP4_ADDR(&ipaddr, IP_ADDR0, IP_ADDR1, IP_ADDR2, IP_ADDR3);
   IP4_ADDR(&netmask, NETMASK_ADDR0, NETMASK_ADDR1 , NETMASK_ADDR2, NETMASK_ADDR3);
   IP4_ADDR(&gw, GW_ADDR0, GW_ADDR1, GW_ADDR2, GW_ADDR3);
-  
+
   /* - netif_add(struct netif *netif, struct ip_addr *ipaddr,
   struct ip_addr *netmask, struct ip_addr *gw,
   void *state, err_t (* init)(struct netif *netif),
   err_t (* input)(struct pbuf *p, struct netif *netif))
-  
+
   Adds your network interface to the netif_list. Allocate a struct
   netif and pass a pointer to this structure as the first argument.
   Give pointers to cleared ip_addr structures when using DHCP,
   or fill them with sane numbers otherwise. The state pointer may be NULL.
-  
+
   The init function pointer must point to a initialization function for
   your ethernet netif interface. The following code illustrates it's use.*/
-  
+
   netif_add(&gnetif, &ipaddr, &netmask, &gw, NULL, &ethernetif_init, &tcpip_input);
-  
+
   /*  Registers the default network interface. */
   netif_set_default(&gnetif);
-  
+
   if (netif_is_link_up(&gnetif))
   {
     /* When the netif is fully configured this function must be called.*/
@@ -189,28 +198,28 @@ static void BSP_Config(void)
 {
   /* Initialize the LCD */
   BSP_LCD_Init();
-  
+
   /* Initialize the LCD Layers */
   BSP_LCD_LayerDefaultInit(1, LCD_FB_START_ADDRESS);
-  
+
   /* Set LCD Foreground Layer  */
   BSP_LCD_SelectLayer(1);
-  
+
   BSP_LCD_SetFont(&LCD_DEFAULT_FONT);
-  
+
   /* Initialize LCD Log module */
   LCD_LOG_Init();
-  
+
   /* Show Header and Footer texts */
   LCD_LOG_SetHeader((uint8_t *)"Webserver Application Netconn API");
   LCD_LOG_SetFooter((uint8_t *)"STM32746G-DISCO board");
-  
+
   LCD_UsrLog ((char *)"  State: Ethernet Initialization ...\n");
 }
 
 /**
   * @brief  System Clock Configuration
-  *         The system Clock is configured as follow : 
+  *         The system Clock is configured as follow :
   *            System Clock source            = PLL (HSE)
   *            SYSCLK(Hz)                     = 216000000
   *            HCLK(Hz)                       = 216000000
@@ -256,12 +265,12 @@ static void SystemClock_Config(void)
   {
     while(1) { ; }
   }
-  
+
   /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 clocks dividers */
   RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;  
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
   ret = HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7);
@@ -281,7 +290,7 @@ static void SystemClock_Config(void)
 static void MPU_Config(void)
 {
   MPU_Region_InitTypeDef MPU_InitStruct;
-  
+
   /* Disable the MPU */
   HAL_MPU_Disable();
 
@@ -330,7 +339,7 @@ void assert_failed(uint8_t* file, uint32_t line)
 {
   /* User can add his own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  
+
   /* Infinite loop */
   while (1)
   {
